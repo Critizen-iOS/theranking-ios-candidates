7055683ccc71819eee7eb90086574a27cc8679d2
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-11-14 00:31:33 +0100

    Prepares detail vc UI widgets. Refs gradha/theranking-ios-candidates#5.

--- src/vcs/TRPhotoDetailVC.m
+++ src/vcs/TRPhotoDetailVC.m
@@ -1,7 +1,23 @@
 #import "TRPhotoDetailVC.h"
 
+#import "ELHASO.h"
+#import "TRPhotoData.h"
+#import "TRUserData.h"
+
+#import <MapKit/MapKit.h>
+
+
 @interface TRPhotoDetailVC ()
 
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet UILabel *imageNameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *imageDescriptionLabel;
+@property (strong, nonatomic) IBOutlet UIImageView *avatarImageView;
+@property (strong, nonatomic) IBOutlet UILabel *userNameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *userDescriptionLabel;
+@property (strong, nonatomic) IBOutlet UILabel *cameraLabel;
+@property (strong, nonatomic) IBOutlet UILabel *locationLabel;
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
 @end
 
 @implementation TRPhotoDetailVC
@@ -9,12 +25,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    LASSERT(self.photoData, @"Huh, loading view without required data?");
 
     self.title = NSLocalizedString(@"Photo detail", nil);
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]
         initWithTitle:NSLocalizedString(@"Close", nil)
         style:UIBarButtonItemStylePlain target:self
         action:@selector(closeDetailVC)];
+
+    self.imageNameLabel.text = self.photoData.photoName;
+    self.imageDescriptionLabel.text = self.photoData.photoDesc;
+    self.userNameLabel.text = self.photoData.user.combinedName;
+    self.userDescriptionLabel.text = self.photoData.user.descriptionText;
+    self.cameraLabel.text = @"TODO";
+    self.locationLabel.text = @"TODO";
 }
 
 /// The user got tired, bring them back to the thumbnail view.
