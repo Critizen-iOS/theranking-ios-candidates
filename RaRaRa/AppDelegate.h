b0281acc4a278ec4c4a5b7194ed5082290b5624a
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-11-13 16:08:43 +0100

    Adds project from xcode template. Refs gradha/theranking-ios-candidates#1.

---
+++ RaRaRa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RaRaRa
+//
+//  Created by Grzegorz Adam Hankiewicz on 13/11/14.
+//  Copyright (c) 2014 Electric Hands Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RaRaRa/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RaRaRa
+//
+//  Created by Grzegorz Adam Hankiewicz on 13/11/14.
+//  Copyright (c) 2014 Electric Hands Software. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
