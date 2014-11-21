9f6cea6ae62d9b40478d6d1025f3cf1bd4d97f9e
Joaquin Perez Barroso <macalexia@Mac-Pro-de-Joaquin-2.local>
2014-11-21 12:44:40 +0100

    BlankApp

---
+++ theranking-ios-candidates.Tests/theranking_ios_candidates_Tests.m
@@ -0,0 +1,40 @@
+//
+//  theranking_ios_candidates_Tests.m
+//  theranking-ios-candidates.Tests
+//
+//  Created by Joaquin Perez Barroso on 21/11/14.
+//  Copyright (c) 2014 Joaquin Perez Barroso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface theranking_ios_candidates_Tests : XCTestCase
+
+@end
+
+@implementation theranking_ios_candidates_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
