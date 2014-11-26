7814d380d413b4394ca3d514c715f6745fc0fc1d
Rafael Bartolome <rafa@rafaelbartolome.es>
2014-11-26 14:02:10 +0100

    Initial Commit

---
+++ RankTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RankTest
+//
+//  Created by Rafael Bartolome on 26/11/14.
+//  Copyright (c) 2014 www.rafaelbartolome.es. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RankTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RankTest
+//
+//  Created by Rafael Bartolome on 26/11/14.
+//  Copyright (c) 2014 www.rafaelbartolome.es. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
