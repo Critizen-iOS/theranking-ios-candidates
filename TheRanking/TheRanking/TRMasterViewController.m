3d1603c5597d045bfcd396cab5483bd4b1ce291d
Jesús Emilio Fernández de Frutos <jesusemiliofernandezdefrutos@Jesuss-MacBook-Pro.local>
2015-02-07 15:53:50 +0100

    Refresh data

--- TheRanking/TheRanking/TRMasterViewController.m
+++ TheRanking/TheRanking/TRMasterViewController.m
@@ -30,12 +30,12 @@ NSString *const myCell					= @"Cell";
     AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication] delegate];
     _managedObjectContext = [appDelegate managedObjectContext];
     
-    [self configureCollection];
+    [self configureLayout];
     
     [self loadData];
 }
 
--(void) configureCollection
+-(void) configureLayout
 {
     UINib *cellNib = [UINib nibWithNibName:@"TRPhotoCollectionViewCell" bundle:nil];
     [self.collectionView registerNib:cellNib forCellWithReuseIdentifier:myCell];
@@ -44,6 +44,13 @@ NSString *const myCell					= @"Cell";
     UICollectionViewFlowLayout *flowLayout = (UICollectionViewFlowLayout*)self.collectionView.collectionViewLayout;
     [flowLayout setItemSize:CGSizeMake(110, 124)];
     [flowLayout setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+    
+    UIBarButtonItem *button = [[UIBarButtonItem alloc]
+                               initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh
+                               target:self
+                               action:@selector(refreshButtonSend)];
+    self.navigationItem.rightBarButtonItem = button;
+    [self.navigationItem setTitle:@"TheRanking"];
 }
 
 -(void)loadData
@@ -51,15 +58,71 @@ NSString *const myCell					= @"Cell";
     TRAPIDownloadImage *download= [[TRAPIDownloadImage sharedInstance] init];
     NSLog(@"INIT%@", [download description]);
     
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    if (![defaults objectForKey:@"firstRun"])
+    {
+        
+        [defaults setObject:[NSDate date] forKey:@"firstRun"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+        
+        [self refresh];
+    }
+    
+}
+
+-(void) clearStorage
+{
+    NSFetchRequest * allPhotos = [[NSFetchRequest alloc] init];
+    [allPhotos setEntity:[NSEntityDescription entityForName:@"Photo" inManagedObjectContext:self.managedObjectContext]];
+    [allPhotos setIncludesPropertyValues:NO]; //only fetch the managedObjectID
+    
+    NSError * error = nil;
+    NSArray * photos = [self.managedObjectContext executeFetchRequest:allPhotos error:&error];
+    
+
+    for (NSManagedObject * photo in photos) {
+        [self.managedObjectContext deleteObject:photo];
+    }
+    
+    NSFetchRequest * allUsers = [[NSFetchRequest alloc] init];
+    [allUsers setEntity:[NSEntityDescription entityForName:@"User" inManagedObjectContext:self.managedObjectContext]];
+    [allUsers setIncludesPropertyValues:NO]; //only fetch the managedObjectID
+    
+    NSArray * users = [self.managedObjectContext executeFetchRequest:allPhotos error:&error];
+    
+    for (NSManagedObject * user in users) {
+        [self.managedObjectContext deleteObject:user];
+    }
+
+    
+    
+    NSError *saveError = nil;
+    [self.managedObjectContext save:&saveError];
+}
+
+-(void) refreshButtonSend
+{
+    [self clearStorage];
+    [self refresh];
+}
+
+-(void) refresh
+{
     [[TR500PX sharedInstance] loadDataCompletionBlock:^{
         [self.collectionView reloadData];
     }];
 }
 
-
 - (NSFetchedResultsController *)fetchedResultsController
 {
     if (_fetchedResultsController != nil) {
+        NSError *error = nil;
+        if (![_fetchedResultsController performFetch:&error]) {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
         return _fetchedResultsController;
     }
     
@@ -108,6 +171,7 @@ NSString *const myCell					= @"Cell";
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
     id <NSFetchedResultsSectionInfo> sectionInfo = [self.fetchedResultsController sections][section];
+    NSLog(@"%lu", (unsigned long)[sectionInfo numberOfObjects]);
     return [sectionInfo numberOfObjects];
 }
 
